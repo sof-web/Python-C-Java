6c310a8c9389e73a178653e914e9e84f2073ab72
sof-web <fikegaso814@gmail.com>
2025-09-20 20:08:37 +0300

    Create my.cpp

---
+++ my.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <list>
+#include <stack> // нужно добавить для работы со стеком
+#include <string> // нужно добавить для работы со строками
+
+int main() {
+    // Создание списка целых чисел
+    std::list<int> numbers = {1, 2, 3, 4, 5};
+    
+    // Создание стека строк (неполный код)
+    std::stack<std::string> stack;
+    // Здесь должны быть операции push(), pop() и т.д.
+}
