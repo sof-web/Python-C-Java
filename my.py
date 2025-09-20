4fff1210d81bd0f1f657caa0273a7e16351f1981
sof-web <fikegaso814@gmail.com>
2025-09-20 20:12:33 +0300

    Update my.py

--- my.py
+++ my.py
@@ -1,13 +1,9 @@
-# создание списка из строки 'abcdef'
-txt = 'abcdef'
-lst = list(txt)  # ['a', 'b', 'c', 'd', 'e', 'f']
+#создание списка и строки
+'abcdef', lst = list(txt)
 
-# организация стека (используя список)
-stack = []
-stack.append('a')
-stack.append('b') 
-stack.append('c')
-print(stack)  # Вывод: ['a', 'b', 'c']
-
-# Для извлечения из стека можно использовать pop()
-last_element = stack.pop()  # удаляет и возвращает 'c
+#организация стека
+stack = list()
+stack.append ('a')
+stack.append ('b')
+stack.append ('c')
+print(stack)
