f3764966e633c2c4ab77d91ac1b2470928e54c16
sof-web <fikegaso814@gmail.com>
2025-09-20 20:11:34 +0300

    Update my.java

--- my.java
+++ my.java
@@ -1,24 +1,11 @@
-
+//создание пустого списка и стека на Java
 import java.util.ArrayList;
-import java.util.Stack; // нужно импортировать
+import java.util.List;
+import java.util.Stack;
 
-class Main {
-    public static void main(String[] args) { // исправлено args[]
-        // Создаём пустой список
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        
-        // Добавляем элементы
-        a.add(1);
-        a.add(2);
-        a.add(3);
-        
-        // Печатаем список
-        System.out.println(a);  // [1, 2, 3]
-        
-        // Создание стека (неполный код)
-        Stack<Integer> stack = new Stack<>(); // исправлено
-        stack.push(10);
-        stack.push(20);
-        // Должны быть операции pop(), peek() и т.д.
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> mylist = new ArrayList<>(); //пустой список
+        Stack<Integer> mystack = new Stack<>(); //пустой стек
     }
 }
